ec4fac1d573c0cee81b7d5d8251340d3dde698a4
Elise <ellie16.ed@gmail.com>
2025-09-10 07:05:20 -0400

    Initial commit

---
+++ src/components/Jumbotron.jsx
@@ -0,0 +1,39 @@
+import React from "react";
+import iPhone from "../assets/images/iphone-14.jpg";
+import HoldingiPhone from "../assets/images/iphone-hand.png";
+
+function Jumbotron() {
+  const handleLearnMore = () => {
+    const element = document.querySelector(".sound-section");
+    window.scrollTo({
+      top: element?.getBoundingClientRect().top,
+      left: 0,
+      behavior: "smooth",
+    });
+  };
+
+  return (
+    <div className="jumbotron-section wrapper">
+      <h2 className="title">New</h2>
+      <img className="logo" src={iPhone} alt="iPhone 14 Pro" />
+      <p className="text">Big and Bigger</p>
+      <span className="description">
+        From $41.62/mo. from 24 mo. or $999 before trade-in
+      </span>
+      <ul className="links">
+        <li>
+          <button className="button">Buy</button>
+        </li>
+        <li>
+          <a className="link" onClick={handleLearnMore}>
+            {" "}
+            Learn More →{" "}
+          </a>
+        </li>
+      </ul>
+      <img className="iphone-img" src={HoldingiPhone} alt="iPhone" />
+    </div>
+  );
+}
+
+export default Jumbotron;
